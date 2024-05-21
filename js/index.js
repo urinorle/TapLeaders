51a5d4c067de58fff978cd381d0e67451a3394d4
Mohamed Zekhnini Abjij <e.mzekhninia@ies-sabadell.cat>
2024-05-21 18:38:40 +0200

    warff

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const clickButton = document.getElementById('click-button');
     const userScoreElement = document.getElementById('user-score');
     const leaderScoreElement = document.getElementById('leader-score');
-    const secondScoreElement = document.getElementById('second-score');
-    const thirdScoreElement = document.getElementById('third-score');
 
     let userScore = 0;
 
@@ -17,15 +15,11 @@ document.addEventListener('DOMContentLoaded', () => {
     function updateScores() {
         let scores = [
             { name: 'User', score: userScore },
-            { name: 'Leader', score: parseInt(leaderScoreElement.textContent) },
-            { name: 'Second', score: parseInt(secondScoreElement.textContent) },
-            { name: 'Third', score: parseInt(thirdScoreElement.textContent) }
+            { name: 'Leader', score: parseInt(leaderScoreElement.textContent) }
         ];
 
         scores.sort((a, b) => b.score - a.score);
 
         leaderScoreElement.textContent = scores[0].score;
-        secondScoreElement.textContent = scores[1].score;
-        thirdScoreElement.textContent = scores[2].score;
     }
 });
